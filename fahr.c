036974ed56519962961fcbee46e7bded2d21a750
orex2105 <pokomisa55@gmail.com>
2026-02-16 13:35:12 +0300

    fahr.c

--- fahr.c
+++ fahr.c
@@ -11,10 +11,12 @@ int main() {
     upper = 300; /* верхняя граница */
     step = 20; /* шаг */
 
+    /* печать таблицы температур по Фаренгейту и Цельсию */
+    printf("%3s\t%6s\n", " ℉", "°C ");
     fahr = lower;
     while (fahr <= upper) {
         celsius = (5.0 / 9.0) * (fahr-32.0);
-        printf("%3.0f %6.2f\n", fahr, celsius);
+        printf("%3.0f\t%6.2f \n", fahr, celsius);
         fahr = fahr + step;
     }
 
