cb152a42225224913f0438448d4843a3827afccb
orex2105 <pokomisa55@gmail.com>
2026-02-17 23:45:56 +0300

    1.5

--- 1.5/tasks.c
+++ 1.5/tasks.c
@@ -30,9 +30,28 @@ void task_two(void) {
     }
 }
 
+void task_three(void) {
+    int ch;
+
+    while ((ch = getchar()) != EOF) {
+        if (ch == '\t') {
+            putchar('\\');
+            putchar('t');
+        } else if (ch == '\b') {
+            putchar('\\');
+            putchar('b');
+        } else if (ch == '\\') {
+            putchar('\\');
+            putchar('\\');
+        } else {
+            putchar(ch);
+        }
+    }
+}
+
 int main(void)
 {
-    task_two();
+    task_three();
 
     return 0;
 }
